0dd0db994e2e2bee1c91ccec93e0750e80d85054
Raviv Barzilay <raviv.barzilay@gmail.com>
2017-07-09 19:00:29 +0300

    added committee meeting attendee support

--- knesset_data_django/committees/models.py
+++ knesset_data_django/committees/models.py
@@ -1,2 +1,2 @@
 # currently uses Open Knesset models. TODO: move to a separate data access layer
-from committees.models import Committee, CommitteeMeeting, ProtocolPart
+from committees.models import Committee, CommitteeMeeting, ProtocolPart, CommitteeMeetingAttendee
--- knesset_data_django/committees/scrapers/committee_meeting_protocols.py
+++ knesset_data_django/committees/scrapers/committee_meeting_protocols.py
@@ -1,6 +1,6 @@
 from ...common.scrapers.base_datapackage_scraper import BaseDatapackageScraper
 from ...mks.utils import get_all_mk_names
-from ..models import CommitteeMeeting, ProtocolPart
+from ..models import CommitteeMeeting, ProtocolPart, CommitteeMeetingAttendee
 from datetime import datetime
 import os
 import csv
@@ -16,7 +16,7 @@ class CommitteeMeetingProtocolsScraper(BaseDatapackageScraper):
             setattr(self, 'loaded_mk_names', True)
         return self.mks, self.mk_names
 
-    def _validate_datapackage_item(self, text_file_path, parts_file_path, meetings_cnt):
+    def _validate_datapackage_item(self, text_file_path, parts_file_path, attendees_file_path, meetings_cnt):
         if meetings_cnt == 0:
             return False, "could not find meeting in DB"
         elif meetings_cnt > 1:
@@ -25,6 +25,8 @@ class CommitteeMeetingProtocolsScraper(BaseDatapackageScraper):
             return False, "missing text file due to error in datapackage creation, see scraper_errors field in datapackage"
         elif parts_file_path == self._get_datapackage_resource_path():
             return False, "missing parts file due to error in datapackage creation, see scraper_errors field in datapackage"
+        elif attendees_file_path == self._get_datapackage_resource_path():
+            return False, "missing attendees file due to error in datapackage creation, see scraper_errors field in datapackage"
         elif not os.path.exists(text_file_path):
             return False, "missing text file {}".format(text_file_path)
         elif not os.path.exists(parts_file_path):
@@ -85,14 +87,37 @@ class CommitteeMeetingProtocolsScraper(BaseDatapackageScraper):
                 attended_ok, attended_message = False, "failed to updated attending members: {}".format(e)
         return protocol_ok, protocol_message, attended_ok, attended_message
 
+    def _save_attendees(self, meeting, attendees):
+        CommitteeMettingAttendees.objects.bulk_create(attendees)
+        meeting.save()
+
+    def _update_attendees(self, meeting, attendees_file_path):
+        attendees_ok, attendees_message = None, None
+        if meeting.attendees.count() > 0:
+            attendees_ok, attendees_message = False, "meeting has existing attendees, will not reparse"
+        else:
+            attendees_ok = True
+            with open(attendees_file_path) as f:
+                attendees = csv.reader(f)
+                assert parts.next() == ['name','role','additional_information']
+                committee_attendees = [CommitteeMeetingAttendee(meeting=meeting,name=attendee[0],role=attendee[1],additional_information=attendee[2])
+                                        for attendee in enumerate(attendees)]
+                self._save_attendees(meeting,committee_attendees)
+                attendees_message = "inserted attendees"
+
+        return attendees_ok, attendees_message
+
     def _handle_datapackage_item(self, meeting_data):
         text_updated, text_message = None, None
         parts_updated, parts_message = None, None
         attended_updated, attended_message = None, None
+        #sorry for this redundancy, i don't want to break stuff (@beber)
+        attendees_updated, attendees_message = None, None
         text_file_path = self._get_datapackage_resource_path(meeting_data["text"])
         parts_file_path = self._get_datapackage_resource_path(meeting_data["parts"])
+        attendees_file_path = self._get_datapackage_resource_path(meeting_data["attendees"])
         cnt, qs = self._get_meetings(meeting_data["committee_id"], meeting_data["meeting_id"])
-        ok, error = self._validate_datapackage_item(text_file_path, parts_file_path, cnt)
+        ok, error = self._validate_datapackage_item(text_file_path, parts_file_path, attendees_file_path, cnt)
         if ok:
             meeting = qs.first()
             text_updated, text_message = self._update_protocol_text(meeting, text_file_path)
@@ -100,9 +125,11 @@ class CommitteeMeetingProtocolsScraper(BaseDatapackageScraper):
                 parts_updated, parts_message, attended_updated, attended_message = False, "protocol text not updated, so skipping parts updating as well", None, None
             else:
                 parts_updated, parts_message, attended_updated, attended_message = self._update_protocol_parts(meeting, parts_file_path)
-        return ok, error, meeting_data, text_updated, text_message, parts_updated, parts_message, attended_updated, attended_message
 
-    def log_return_value(self, ok, error, meeting_data, text_updated, text_message, parts_updated, parts_message, attended_updated, attended_message):
+            attendees_updated, attendees_message = self._update_attendees(meeting, attendees_file_path)
+        return ok, error, meeting_data, text_updated, text_message, parts_updated, parts_message, attended_updated, attended_message, attendees_updated, attendees_message
+
+    def log_return_value(self, ok, error, meeting_data, text_updated, text_message, parts_updated, parts_message, attended_updated, attended_message, attendees_updated, attendees_message):
         if ok:
             self.logger.debug(text_message)
             self.logger.debug(parts_message)
@@ -114,5 +141,12 @@ class CommitteeMeetingProtocolsScraper(BaseDatapackageScraper):
                 self.logger.error("committee {} meeting {}: updated committee meeting protocol parts only".format(meeting_data["committee_id"], meeting_data["meeting_id"]))
             else:
                 self.logger.debug("committee {} meeting {}: no update to protocol text or parts".format(meeting_data["committee_id"], meeting_data["meeting_id"]))
+
+            #fuck this spaghetti (@beber)
+            if attendees_updated:
+                self.logger.info("committee {} meeting {}: updated meeting attendees".format(meeting_data["committee_id"], meeting_data["meeting_id"]))
+            else:
+                self.logger.debug("committee {} meeting {}: no update to meeting attendees".format(meeting_data["committee_id"], meeting_data["meeting_id"]))
+
         else:
             self.logger.info("committee {} meeting {}: error scraping committee meeting protocol: {}".format(meeting_data["committee_id"], meeting_data["meeting_id"], error))
